1bb1f412e864f63d7addc738c92d8013b5cdfbc9
Dido05 <60329186+Dido05@users.noreply.github.com>
2026-04-12 22:18:25 +0300

    Модифициране на Main да чете команди от входен файл и да записва резултатите в изходен файл, вместо да се използва конзолата

--- Hotel/src/bg/tu_varna/sit/f24621715/Main.java
+++ Hotel/src/bg/tu_varna/sit/f24621715/Main.java
@@ -1,90 +1,101 @@
 package bg.tu_varna.sit.f24621715;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Hotel hotel = new Hotel();
 
-        // example rooms
         hotel.addRoom(new Room(101, 2));
         hotel.addRoom(new Room(102, 3));
         hotel.addRoom(new Room(103, 4));
 
-        Scanner sc = new Scanner(System.in);
+        try {
+            Scanner sc = new Scanner(new File("input.txt"));
+            PrintWriter out = new PrintWriter("output.txt");
 
-        while (true) {
-            String command = sc.next();
+            while (sc.hasNext()) {
+                String command = sc.next();
 
-            if (command.equals("checkin")) {
-                int roomNumber = sc.nextInt();
-                LocalDate from = LocalDate.parse(sc.next());
-                LocalDate to = LocalDate.parse(sc.next());
-                String note = sc.next();
-                int guests = sc.nextInt();
+                if (command.equals("checkin")) {
+                    int roomNumber = sc.nextInt();
+                    LocalDate from = LocalDate.parse(sc.next());
+                    LocalDate to = LocalDate.parse(sc.next());
+                    String note = sc.next();
+                    int guests = sc.nextInt();
 
-                Stay stay = new Stay(from, to, note, guests);
+                    Stay stay = new Stay(from, to, note, guests);
 
-                boolean success = hotel.checkIn(roomNumber, stay);
-                System.out.println(success ? "OK" : "Room is not available");
-            }
+                    boolean success = hotel.checkIn(roomNumber, stay);
+                    out.println(success ? "OK" : "Room is not available");
+                }
 
-            else if (command.equals("availability")) {
-                LocalDate from = LocalDate.parse(sc.next());
-                LocalDate to = LocalDate.parse(sc.next());
+                else if (command.equals("availability")) {
+                    LocalDate from = LocalDate.parse(sc.next());
+                    LocalDate to = LocalDate.parse(sc.next());
 
-                for (Room r : hotel.getAvailableRooms(from, to)) {
-                    System.out.println(r.getNumber());
+                    for (Room r : hotel.getAvailableRooms(from, to)) {
+                        out.println(r.getNumber());
+                    }
+                }
+
+                else if (command.equals("find")) {
+                    int beds = sc.nextInt();
+                    LocalDate from = LocalDate.parse(sc.next());
+                    LocalDate to = LocalDate.parse(sc.next());
+
+                    Room room = hotel.findRoom(beds, from, to);
+                    out.println(room != null ? room.getNumber() : "No room");
+                }
+
+                else if (command.equals("find!")) {
+                    int beds = sc.nextInt();
+                    LocalDate from = LocalDate.parse(sc.next());
+                    LocalDate to = LocalDate.parse(sc.next());
+
+                    Room room = hotel.findSpecial(beds, from, to);
+                    out.println(room != null ? room.getNumber() : "No room");
+                }
+
+                else if (command.equals("report")) {
+                    LocalDate from = LocalDate.parse(sc.next());
+                    LocalDate to = LocalDate.parse(sc.next());
+
+                    List<String> report = hotel.report(from, to);
+
+                    for (String line : report) {
+                        out.println(line);
+                    }
+                }
+
+                else if (command.equals("unavailable")) {
+                    int roomNumber = sc.nextInt();
+                    LocalDate from = LocalDate.parse(sc.next());
+                    LocalDate to = LocalDate.parse(sc.next());
+                    String note = sc.next();
+
+                    UnavailablePeriod period = new UnavailablePeriod(from, to, note);
+
+                    boolean success = hotel.makeUnavailable(roomNumber, period);
+                    out.println(success ? "OK" : "Cannot mark unavailable");
+                }
+
+                else if (command.equals("exit")) {
+                    break;
                 }
             }
 
-            else if (command.equals("find")) {
-                int beds = sc.nextInt();
-                LocalDate from = LocalDate.parse(sc.next());
-                LocalDate to = LocalDate.parse(sc.next());
+            out.close();
 
-                Room room = hotel.findRoom(beds, from, to);
-                System.out.println(room != null ? room.getNumber() : "No room");
-            }
-
-            else if (command.equals("find!")) {
-                int beds = sc.nextInt();
-                LocalDate from = LocalDate.parse(sc.next());
-                LocalDate to = LocalDate.parse(sc.next());
-
-                Room room = hotel.findSpecial(beds, from, to);
-                System.out.println(room != null ? room.getNumber() : "No room");
-            }
-
-            else if (command.equals("report")) {
-                LocalDate from = LocalDate.parse(sc.next());
-                LocalDate to = LocalDate.parse(sc.next());
-
-                List<String> report = hotel.report(from, to);
-
-                for (String line : report) {
-                    System.out.println(line);
-                }
-            }
-
-            else if (command.equals("unavailable")) {
-                int roomNumber = sc.nextInt();
-                LocalDate from = LocalDate.parse(sc.next());
-                LocalDate to = LocalDate.parse(sc.next());
-                String note = sc.next();
-
-                UnavailablePeriod period = new UnavailablePeriod(from, to, note);
-
-                boolean success = hotel.makeUnavailable(roomNumber, period);
-                System.out.println(success ? "OK" : "Cannot mark unavailable");
-            }
-
-            else if (command.equals("exit")) {
-                break;
-            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
         }
-
     }
 }
